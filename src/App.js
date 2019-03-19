5791bc84a788bd336895cf8f3a6f754bbe6c423c
Halocline <mdglissmann@gmail.com>
2019-03-19 10:15:23 -0600

    Modifying project data

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Matt Glissmann's Project Portfolio</h1>
         </header>
         <Portfolio />
         <footer className="App-footer">
